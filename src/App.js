fcdf092721149cd3bd6e69138640670781deeeb2
minsung <smsjh2004@naver.com>
2022-05-28 01:43:03 +0900

    fix page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import React from 'react';
+import './App.css'
 
-function App() {
-  return;
+export default function Navibutton() {
+  const Navi = () => {
+    Kakao.isInitialized();
+    Kakao.Navi.start({
+      name: '민성이집',
+      x: 127.099766772087,
+      y: 37.2017555955945,
+      coordType: 'wgs84',
+    })
+  }
+  return(
+    <div>
+      <h1>맞짱뜰사람 컴온</h1>
+      <a href='' onClick={Navi}>민성이집 가는법</a>
+    </div>
+  );
 }
-
-export default App;
